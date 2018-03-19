ebfbe0cef2c9f4303aa91af10667c0d62621fa78
Ahmed Magdy Abd-alzaher <ahmed.magdy.abd.alzaher@gmail.com>
2018-03-19 17:59:11 +0200

    Initial commit

---
+++ app/src/main/java/com/find/doctor/MainActivity.java
@@ -0,0 +1,42 @@
+package com.find.doctor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    TranslateAnimation anim1,anim2;
+    Button mNearHospital, mStarHospital;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        anim1 = new TranslateAnimation(-1000f, 0f, 0f, 0f);
+        anim1.setDuration(1000);
+        anim2 = new TranslateAnimation(1000f, 0f, 0f, 0f);
+        anim2.setDuration(1000);
+        mNearHospital = (Button) findViewById(R.id.nearHospital);
+        mStarHospital = (Button) findViewById(R.id.starHospital);
+        mNearHospital.setAnimation(anim1);
+        mNearHospital.setVisibility(View.VISIBLE);
+        mStarHospital.setAnimation(anim2);
+        mStarHospital.setVisibility(View.VISIBLE);
+        mNearHospital.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+        mStarHospital.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+    }
+}
